8706422b6a9202bcacd18167c24eb889cb8f4339
ahollin5 <ahollin5@asu.edu>
2021-12-04 12:48:54 -0500

    sizing dates and css consolidation

---
+++ javascript/thankyou.js
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>cross-stitchOBSESSED</title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="css/obsessed.css">
+      <link rel="stylesheet" href="css/headnav.css">
+      <link rel="stylesheet" href="css/projectplanning.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+      <header>cross-stitchOBSESSED</header>
+            <nav>
+         <a href="index.html">Home</a>
+         <div class="dropdown">
+            <button class="dropbtn">Dashboard <span class="smallarrow">&#9660;</span>	
+            </button>
+            <div class="dropdown-content">
+               <a href="dashboard.html">My Dashboard</a>
+               <a href="dashboard.html">Floss Inventory</a>
+            </div>
+         </div>
+         <div class="dropdown">
+            <button class="dropbtn">Project Planning <span class="smallarrow">&#9660;</span></button>
+            <div class="dropdown-content">
+               <a href="projectplanning.html">Walkthrough</a>
+               <a href="projectplanning.html">Tools</a>
+            </div>
+         </div>
+         <a href="materials.html">Materials</a>
+         <a href="howto.html">How-To</a>
+      </nav>
+      <div class="wrapper">
+         <div class="twocolcontainer">
+            <div class="one">
+               <h1 class="headertext">Aida</h1>
+            </div>
+            <div class="two"><img src="media/aida.jpeg"></div>
+         </div>
+         <div class="purpleblock">
+            <h1>Pros and Cons</h1>
+         </div>
+         <table>
+            <tbody>
+               <tr>
+                  <th>
+                     <h2>Pros</h2>
+                      
+                  </th>
+               </tr>
+               <tr>
+                  <td>
+                     <ul>
+                          <li>Aida ranges greatly in thread count from 11 down to 18 count. Making it a very versatile fabric.</li>
+                         <li>Because of the very open weave, the holes are very easy to see, making it great for beginners or those who might not have the best eye sight.
+                         </li>
+                         <li>This fabric is very widely used across the cross-stitching community, so its very easy to find in crafting stores and readily available.</li>
+                      </ul>
+                  </td>
+               </tr>
+                 <tr>
+                  <th>
+                     <h2>Cons</h2>
+                  </th>
+               </tr>
+               <tr>
+                  <td>
+                     <ul>
+                          <li>Because the holes are easy to see, it also makes the finished cross-stitch project have a coarser finished look. Some people love this, but others prefer the way evenweave or linen looks better when finished</li>
+                         <li>Aida isnt very easy to use for advanced cross stitch projects that use 1/4 or 3/4 stitches</li>
+                         <li>Because the thread count only gets as small as 20 count, Aida isnt suited for very large projects (unless you want the project to be very physically large)</li>
+                      </ul>
+                  </td>
+               </tr>
+            </tbody>
+          </table>
+         
+        </div>
+        <footer>Questions or content requests? Please <a href="contactus.html">contact us</a>
+      <br>
+      Copyright &copy; 2021 cross-stitchOBSESSED
+   </footer>
+   </body>
+</html>
